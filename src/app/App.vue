1d8d272b4991bd38e31660c5bea3dd902f369f5d
avvet <sveta.drozdovich@yandex.ru>
2018-04-27 14:02:26 +0300

    filter created

---
+++ src/app/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="'app">
+    <!--<h1>App vue rendered! {{msg}}</h1>-->
+    <!--<img style="width:140px; margin:14px;" src="./assets/img/logo.png"/>-->
+    <image-container></image-container>
+    <todo-list></todo-list>
+    <!--<img src="../assets/img/familyguy.png" width="100" height="300" alt="">-->
+  </div>
+</template>
+
+<script>
+  import ImageContainerComponent from "Components/image-container-component/ImageContainerComponent";
+  import todoList from "Components/todo-list-component/todoList.vue";
+
+  export default {
+    data() {
+      return {
+        // msg: "DataMsg"
+      }
+    },
+    name: 'App',
+    components: {
+      'image-container': ImageContainerComponent,
+      'todo-list':todoList
+    }
+  }
+</script>
+
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+</style>
---
+++ src/app/components/todo-list-component/todoList.vue
@@ -0,0 +1,58 @@
+<template src="./todoList.html"></template>
+<style src="./todoList.scss" scoped lang="scss"></style>
+
+
+<script>
+  import axios from "axios";
+
+  let todos = [
+    {
+      "name": "task 2",
+      "completed": false
+    }
+  ];
+  export default {
+    name: 'todoList',
+    data () {
+      return {
+        title: 'TODO list',
+        todos,
+        newToDo:''
+      }
+    },
+    methods:{
+      addToDo(){
+        let newTodo = {
+          completed: false,
+          name: this.newToDo
+        };
+
+        this.todos.push(newTodo);
+        this.onSaveDataToServer(newTodo);
+        this.newToDo ='';
+      },
+      onSaveDataToServer(newTodo) {
+        axios
+          .post(url, newTodo)
+      },
+      remaining(){
+        let compl = document.getElementById('phrase');
+        let amount = this.todos.filter(todo => !todo.completed).length;
+        compl.innerHTML = amount + ' ' + 'tasks';
+        compl.style.color='red';
+      },
+      done(){
+        let done = document.getElementById('phrase');
+        let amount2 = this.todos.filter(todo => todo.completed).length;
+        done.innerHTML = amount2 + ' ' + 'tasks';
+        done.style.color='darkcyan';
+      }
+    },
+    // computed:{
+    //   remaining(){
+    //     return this.todos.filter(todo => !todo.completed).length;
+    //   }
+    // }
+  }
+</script>
+
