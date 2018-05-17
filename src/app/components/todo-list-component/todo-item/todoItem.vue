07a5acc68cd8393634583cf191e9821815469d6a
avvet <sveta.drozdovich@yandex.ru>
2018-05-17 10:58:43 +0300

    all fixes made

--- src/app/components/todo-list-component/todo-item/todoItem.vue
+++ src/app/components/todo-list-component/todo-item/todoItem.vue
@@ -30,6 +30,9 @@
     props:[
       'singleTodoItem'
     ],
+    created(){
+      this.editInputContent = this.singleTodoItem.title;
+    },
     methods:{
       onDeleteItem(){
         let singleId = this.singleTodoItem.id;
@@ -59,6 +62,56 @@
     margin-top: 5px;
     border: 1px solid #8d81d6;
     padding-left: 10px;
+    font-size: 16px;
+  }
+  .todo_item{
+    z-index: 1;
+    float: left;
+    width: 100%;
+    border-radius:3px;
+    background-color: white;
+    height: 50px;
+    margin-bottom: 15px;
+    -webkit-box-shadow: 4px 3px 13px -5px rgba(0,0,0,0.25);
+    -moz-box-shadow: 4px 3px 13px -5px rgba(0,0,0,0.25);
+    box-shadow: 4px 3px 13px -5px rgba(0,0,0,0.25);
+    transition: box-shadow .3s;
+    &:hover {
+      -webkit-box-shadow: 4px 3px 13px -5px rgba(0,0,0,0650);
+      -moz-box-shadow: 4px 3px 13px -5px rgba(0,0,0,0.60);
+      box-shadow: 4px 3px 13px -5px rgba(0,0,0,0.60);
+    }
+    .checkbox{
+      float: left;
+      width: 15px;
+      height: 15px;
+      margin-right: 20px;
+      margin-top: 15px;
+      margin-left: 12px;
+      border-radius: 3px;
+      border: 1px solid #8d81d6;
+    }
+    .checked{
+      background-color: lightgray;
+    }
+    .item_title{
+      float: left;
+      margin: 0;
+      line-height: 50px;
+      font-family: Roboto;
+    }
+    .btn_container{
+      float: right;
+      margin-top: 0px;
+      margin-right: 20px;
+      .btn{
+        float: left;
+        margin-right: 20px;
+        font-family: Roboto;
+        line-height: 50px;
+        font-size: 14px;
+      }
+    }
   }
 
 </style>
